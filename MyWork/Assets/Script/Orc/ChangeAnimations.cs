b50617341844df61ba864e091b259f89745556e4
Unknown <nerorus97@gmail.com>
2019-03-08 22:20:03 +0400

    Commit 08.03.2019
    Добавил управление мышью.
    Скачал доп. библиотеки( при финальной сборке оставлю только то что необходимо)

---
+++ MyWork/Assets/Script/Orc/ChangeAnimations.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimations : MonoBehaviour
+{
+    public GameObject _movePoint;
+    private Animator anim;
+    public bool _walk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        _walk = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_walk)
+        {
+            anim.SetBool("IsRunnig", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunnig", false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == _movePoint.tag)
+        {
+            _walk = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == _movePoint.tag)
+        {
+            _walk = true;
+        }
+    }
+}
